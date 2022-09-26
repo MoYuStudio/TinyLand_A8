e6ef46f64d8ca7b288df2e644e9d7d76954dc466
WilsonVinson <cprewilsonvinson@gmail.com>
2022-09-26 17:51:06 +0800

    main

--- project/object/script/tilemap.gd
+++ project/object/script/tilemap.gd
@@ -11,4 +11,4 @@ func _process(delta):
 		# update_dirty_quadrants()
 	if Input.get_mouse_button_mask() == 2:
 		var mouse_pos = get_global_mouse_position()
-		set_cell(world_to_map(mouse_pos)[0],world_to_map(mouse_pos)[1], 0)
+		set_cell(world_to_map(mouse_pos)[0],world_to_map(mouse_pos)[1], -1)
