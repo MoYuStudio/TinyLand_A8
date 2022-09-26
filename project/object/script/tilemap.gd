11ee38a40c73232d0d4dcf20ae6b47ccabce5329
WilsonVinson <cprewilsonvinson@gmail.com>
2022-09-26 18:07:44 +0800

    main

--- project/object/script/tilemap.gd
+++ project/object/script/tilemap.gd
@@ -1,8 +1,27 @@
 extends TileMap
 
 func _ready():
-	set_cell(3, 3, 1)
-	pass
+	var noise = OpenSimplexNoise.new()
+	randomize()
+	# Configure the OpenSimplexNoise instance.
+	noise.seed = 10000
+	noise.octaves = 4
+	noise.period = 20.0
+	noise.persistence = 0.8
+
+	#for i in 100:
+		# Prints a slowly-changing series of floating-point numbers
+		# between -1.0 and 1.0.
+		#print(noise.get_noise_2d(i))
+	
+	var tilemap_board = 32
+	for x in range(tilemap_board):
+		for y in range(tilemap_board*2):
+			var the_noise = noise.get_noise_2d(x,y)*100+10
+			if the_noise >= 0:
+				set_cell(x, y, 0)
+			if the_noise <= 0:
+				set_cell(x, y, 1)
 
 func _process(delta):
 	if Input.get_mouse_button_mask() == 1:
