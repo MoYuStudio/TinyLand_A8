af9047d8646fc133cf8e18d56c97581524fe9d16
WilsonVinson <cprewilsonvinson@gmail.com>
2022-09-26 18:17:56 +0800

    main

--- project/object/script/tilemap.gd
+++ project/object/script/tilemap.gd
@@ -13,10 +13,13 @@ func _ready():
 	for x in range(tilemap_board):
 		for y in range(tilemap_board*2):
 			var the_noise = noise.get_noise_2d(x,y)*100+10
-			if the_noise >= 0:
-				set_cell(x, y, 0)
+			print(the_noise)
 			if the_noise <= 0:
 				set_cell(x, y, 1)
+			elif the_noise <= 10:
+				set_cell(x, y, 2)
+			elif the_noise <= 100:
+				set_cell(x, y, 0)
 
 func _process(delta):
 	if Input.get_mouse_button_mask() == 1:
