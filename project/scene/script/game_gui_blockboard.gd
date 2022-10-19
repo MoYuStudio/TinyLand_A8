f44c41138fe253e7adc1ded028263f2a4c803a7f
WilsonVinson <cprewilsonvinson@gmail.com>
2022-10-19 15:16:13 +0800

    main

--- project/scene/script/game_gui_blockboard.gd
+++ project/scene/script/game_gui_blockboard.gd
@@ -7,8 +7,8 @@ func _ready():
 	pass
 
 func _process(delta):
-	var block_id_button_list = [$Ground/Group/Tile0.block_id,
-								$Ground/Group/Tile1.block_id]
+	var block_id_button_list = [$Ground/Group/ScrollContainer/HBoxContainer/Tile0.block_id,
+								$Ground/Group/ScrollContainer/HBoxContainer/Tile1.block_id]
 	for id in block_id_button_list:
 		if id != null:
 			emit_signal("block_be_choose",id)
