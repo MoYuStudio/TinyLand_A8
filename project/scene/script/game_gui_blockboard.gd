b2ef6c1227164b7ca6c26ad592ea3fe7cb8f8120
WilsonVinson <cprewilsonvinson@gmail.com>
2022-10-20 10:21:24 +0800

    main

--- project/scene/script/game_gui_blockboard.gd
+++ project/scene/script/game_gui_blockboard.gd
@@ -3,12 +3,17 @@ extends Control
 signal block_be_choose(id)
 
 func _ready():
-	# $MenuButton.visible = false
 	pass
 
 func _process(delta):
-	var block_id_button_list = [$Ground/Group/ScrollContainer/HBoxContainer/Tile0.block_id,
-								$Ground/Group/ScrollContainer/HBoxContainer/Tile1.block_id]
-	for id in block_id_button_list:
-		if id != null:
-			emit_signal("block_be_choose",id)
+	
+	
+	if $Ground/Group/ScrollContainer/HBoxContainer/Tile0.block_id != null:
+			emit_signal("block_be_choose",$Ground/Group/ScrollContainer/HBoxContainer/Tile0.block_id)
+			$Ground/Group/ScrollContainer/HBoxContainer/Tile0.block_id = null
+	if $Ground/Group/ScrollContainer/HBoxContainer/Tile1.block_id != null:
+			emit_signal("block_be_choose",$Ground/Group/ScrollContainer/HBoxContainer/Tile1.block_id)
+			$Ground/Group/ScrollContainer/HBoxContainer/Tile1.block_id = null
+	if $Ground/Group/ScrollContainer/HBoxContainer/Tile2.block_id != null:
+			emit_signal("block_be_choose",$Ground/Group/ScrollContainer/HBoxContainer/Tile2.block_id)
+			$Ground/Group/ScrollContainer/HBoxContainer/Tile2.block_id = null
